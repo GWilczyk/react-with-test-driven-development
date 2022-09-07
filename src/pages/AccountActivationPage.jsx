f4de3bd5be20f189d3fe7ebcd10b1c974a56b18a
GWilczyk <gwilczyk.github@gmail.com>
2022-09-07 17:00:48 +0200

    Progress indicator for account activation

--- src/pages/AccountActivationPage.jsx
+++ src/pages/AccountActivationPage.jsx
@@ -6,6 +6,8 @@ const AccountActivationPage = ({ match }) => {
 	const [result, setResult] = useState('')
 
 	useEffect(() => {
+		setResult('')
+
 		activate(match.params.token)
 			.then(response => {
 				if (response.ok) {
@@ -17,16 +19,17 @@ const AccountActivationPage = ({ match }) => {
 			.catch(() => setResult('failed'))
 	}, [match.params.token])
 
-	return (
-		<div data-testid='activation-page'>
-			{result === 'success' && (
-				<div className='alert alert-success mt-3'>Account is activated</div>
-			)}
-			{result === 'failed' && (
-				<div className='alert alert-danger mt-3'>Activation failure</div>
-			)}
-		</div>
-	)
+	let content = <span className='spinner-border' role='status' />
+
+	if (result === 'success') {
+		content = (
+			<div className='alert alert-success mt-3'>Account is activated</div>
+		)
+	} else if (result === 'failed') {
+		content = <div className='alert alert-danger mt-3'>Activation failure</div>
+	}
+
+	return <div data-testid='activation-page'>{content}</div>
 }
 
 export default AccountActivationPage
